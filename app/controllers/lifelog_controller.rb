83def98b8c45c52568279e0bfa5edccd8500414d
Kaito Udagawa <umireon@gmail.com>
2015-02-22 12:47:31 +0900

    refresh token

--- app/controllers/lifelog_controller.rb
+++ app/controllers/lifelog_controller.rb
@@ -21,7 +21,7 @@ class LifelogController < ApplicationController
     client = OAuth2::Client.new(ENV['LIFELOG_CLIENT_ID'], ENV['LIFELOG_CLIENT_SECRET'], site: 'https://platform.lifelog.sonymobile.com/', authorize_url: '/oauth/2/authorize', token_url: '/oauth/2/token')
     token = OAuth2::AccessToken.from_hash(client, JSON.parse(current_user.lifelog_oauth_token))
     if token.expired?
-      token.refresh!
+      token = token.refresh!
       currenc_user.lifelog_oauth_token = token.to_hash.to_json
       current_user.save
     end
